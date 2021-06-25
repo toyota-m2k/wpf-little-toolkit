74eb14dcc41bb55237718c86cd0836f869981e6c
toyota-m2k <toyota.mitsuki@gmail.com>
2021-06-25 10:02:56 +0900

    disposable pool を実装

---
+++ wpf-little-library/utils/DisposablePool.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace io.github.toyota32k.toolkit.utils {
+    public class DisposablePool : List<IDisposable>, IDisposable {
+        public void Dispose() {
+            foreach (var e in this) {
+                e.Dispose();
+            }
+            Clear();
+        }
+    }
+}
