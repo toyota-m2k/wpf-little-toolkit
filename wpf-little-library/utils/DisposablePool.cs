c0047fa1e7ae65b551c57c0e7f4610c54cc2b7d2
toyota-m2k <toyota.mitsuki@gmail.com>
2024-01-04 08:49:08 +0900

    feat: ユーティリティクラス(CommonProcessor, GenericDisposable, WeakList, Json Extensions)追加

--- wpf-little-library/utils/DisposablePool.cs
+++ wpf-little-library/utils/DisposablePool.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+
 using System.Text;
 using System.Threading.Tasks;
 
 namespace io.github.toyota32k.toolkit.utils {
     public class DisposablePool : List<IDisposable>, IDisposable {
-        public void Dispose() {
+        public void Reset() {
             foreach (var e in this) {
                 e.Dispose();
             }
             Clear();
+        }
+
+        public void Dispose() {
+            Reset();
         }
     }
 }
---
+++ wpf-little-library/utils/GenericDisposable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace io.github.toyota32k.toolkit.utils {
+    internal class GenericDisposable : IDisposable {
+        Action mDisposer;
+
+        public GenericDisposable(Action disposer) {
+            mDisposer = disposer;
+        }
+
+        public void Dispose() {
+            mDisposer?.Invoke();
+            mDisposer = null;
+        }
+
+        public static IDisposable Create(Action creator, Action disposer) {
+            creator?.Invoke();
+            return new GenericDisposable(disposer);
+        }
+        public static IDisposable Create(Func<Action> creator) {
+            var disposer = creator.Invoke();
+            return new GenericDisposable(disposer);
+        }
+    }
+}
